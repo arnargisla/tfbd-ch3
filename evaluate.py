4ec892db392ca3fae7f71e1c2077c4c4882f3cad
Arnar Þór <arnargisla@gmail.com>
2017-11-23 01:07:51 +0000

    random classifier running with make test now

--- evaluate.py
+++ evaluate.py
@@ -5,15 +5,17 @@ def main():
   counter = 0
   classes = {}
   for line in fileinput.input():
-    class_name, observation_name = line.split()
+    class_name, observation_name, extension = [p for s in line.split(' ') for p in s.split('.')]
     if class_name not in classes:
       classes[class_name] = set()
     classes[class_name].add(observation_name)
 
   for class_name in classes:
-      print(class_name, classes[class_name])
+      pass #print(class_name, classes[class_name])
   
-  clusters = list(classes.keys())
+  clusters = list(classes.values())
+  for cluster in clusters:
+    pass #print(cluster)
   rand_index(clusters)
 
 
