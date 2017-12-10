0feee24dae9446446c707138f3bf7666aa9b832a
andberger <and.bergsson@gmail.com>
2017-12-10 18:34:36 +0100

    hashing of videos as an average pixel frame implemented

---
+++ image_hash.py
@@ -0,0 +1,63 @@
+import sys
+from PIL import Image
+import numpy as np
+
+
+def convert_to_grayscale(image):
+    width, height = image.size
+
+    gray_image = Image.new("RGB", (width, height), "white")
+    pixels = gray_image.load()
+
+    for i in range(width):
+        for j in range(height):
+            pixel = image.getpixel((i, j))
+            red, green, blue = pixel[0], pixel[1], pixel[2]
+            gray = (red * 0.299) + (green * 0.587) + (blue * 0.114)
+            pixels[i, j] = (int(gray), int(gray), int(gray))
+
+    return gray_image
+
+def compare_adjacent(pixels):
+    rows, columns = pixels.shape
+    comparison_array = np.empty([rows,columns - 1], dtype=bool)
+    for i in range(rows):
+        for j in range(columns - 1):
+            if pixels[i][j] > pixels[i][j+1]:
+                comparison_array[i][j] = True
+            else:
+                comparison_array[i][j] = False
+
+    return comparison_array
+
+def hash_function(differences):
+    outer_hex_string = ''
+    for difference in differences:
+        decimal_value = 0
+        hex_string = []
+        for index, value in enumerate(difference):
+            if value:
+                decimal_value += 2**(index % 8)
+            if (index % 8) == 7:
+                hex_string.append(hex(decimal_value)[2:].rjust(2, '0'))
+                decimal_value = 0
+        outer_hex_string += ''.join(hex_string)
+    
+    return outer_hex_string
+
+def image_hash(image):
+
+    #Grayscale the image
+    grayscale_image = convert_to_grayscale(image)
+
+    #Resize to fx. 9 x 8 pixels
+    resized_image = grayscale_image.resize((9,8))
+
+    #Compare adjacent values (x > y)
+    pixels = [int((t[0] + t[1] + t[2])/len(t)) for t in list(resized_image.getdata())]
+    adjacent_values_comparison_array = compare_adjacent(np.array(pixels).reshape(8,9))
+
+    #Convert to hash
+    h = hash_function(adjacent_values_comparison_array.tolist())
+    return h
+
--- main.py
+++ main.py
@@ -3,11 +3,39 @@ from random import random
 from math import floor
 from os import listdir
 from os.path import isfile, join
+from moviepy.editor import VideoFileClip
+from image_hash import image_hash
+from PIL import Image
+import numpy as np
+
+def create_video_hash(file_name):
+
+    #Get the video file
+    clip = VideoFileClip(file_name)
+
+    #For each video, create an average frame of the pixels
+    n_frames = 0
+    dimensions = list(reversed(clip.size)) + [3]
+    pixel_matrix = np.zeros(tuple(dimensions), dtype=np.uint8)
+    for frame in clip.iter_frames():
+        pixel_matrix = pixel_matrix + frame
+        n_frames = n_frames + 1
+
+    average_frame_matrix = pixel_matrix / n_frames
+    avg_frame = Image.fromarray(average_frame_matrix, 'RGB')
+
+    #Create an image hash for the average frame
+    h = image_hash(avg_frame)
+    return h
+
+
 
 def main(argv):
     data_path = argv[1]
     for file_name in listdir(data_path):
         if isfile(join(data_path, file_name)):
+            video_hash = create_video_hash(join(data_path, file_name))
+            print(video_hash)
             print((floor(random()*10) % 10), file_name)
 
 if __name__ == "__main__":
